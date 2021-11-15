bbc156cd195086123792dd01a433e7ff5dc9a154
konartur <suntikflood@gmail.com>
2021-11-15 14:16:23 +0200

    created fields and constructor

---
+++ app/impl/Calc.ts
@@ -0,0 +1,11 @@
+class Calc {
+    private _firstNumber: number;
+    private _secondNumber: number;
+    private _result: number;
+
+    public constructor() {
+        this._firstNumber = 0;
+        this._secondNumber = 0;
+        this._result = 0;
+    }
+}
